630c955126345c2d97ed11c5f17af7f5db072363
xuhui-web <578734857@qq.com>
2022-06-30 20:03:20 +0800

    提交新增角色

--- src/api/setting.js
+++ src/api/setting.js
@@ -51,3 +51,14 @@ export function getRoleDetail(id) {
     url: `/sys/role/${id}`
   })
 }
+
+/** *
+ * 新增角色
+ * ***/
+export function addRole(data) {
+  return request({
+    url: '/sys/role',
+    data,
+    method: 'post'
+  })
+}
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -7,7 +7,9 @@
                     <el-tab-pane label="角色管理">
                         <!-- 新增角色按钮 -->
                         <el-row style="height: 60px">
-                            <el-button icon="el-icon-plus" size="small" type="primary">新增角色</el-button>
+                            <el-button icon="el-icon-plus" size="small" type="primary" @click="showDialog = true"
+                                >新增角色</el-button
+                            >
                         </el-row>
                         <!-- 表格 -->
                         <el-table border="" :data="list">
@@ -88,8 +90,8 @@
 </template>
 
 <script>
-import { getRoleList, getCompanyInfo, deleteRole, updateRole, getRoleDetail } from "@/api/setting"
 import { mapGetters } from "vuex"
+import { getRoleList, getCompanyInfo, deleteRole, updateRole, getRoleDetail, addRole } from "@/api/setting"
 
 export default {
     data() {
@@ -117,8 +119,7 @@ export default {
     methods: {
         async getRoleList() {
             const { total, rows } = await getRoleList(this.page)
-            console.log(total)
-            console.log(rows)
+
             this.page.total = total
             this.list = rows
         },
@@ -130,7 +131,6 @@ export default {
         // 获取的公司的信息
         async getCompanyInfo() {
             this.formData = await getCompanyInfo(this.companyId)
-            console.log(this.formData)
         },
         async deleteRole(id) {
             //  提示
@@ -158,17 +158,23 @@ export default {
                     await updateRole(this.roleForm)
                 } else {
                     // 新增业务
+                    await addRole(this.roleForm)
                 }
                 // 重新拉取数据
                 this.getRoleList()
                 this.$message.success("操作成功")
-                this.showDialog = false
+                this.showDialog = false // 关闭弹层  =>  触发el-dialog的事件close事件
             } catch (error) {
                 console.log(error)
             }
         },
-
         btnCancel() {
+            this.roleForm = {
+                name: "",
+                description: "",
+            }
+            // 移除校验
+            this.$refs.roleForm.resetFields()
             this.showDialog = false
         },
     },
