6815b03366e4fb17f957b8f15a06af6e92bdacc5
xuhui-web <578734857@qq.com>
2022-06-30 21:03:39 +0800

    提交新增员工弹层

--- src/main.js
+++ src/main.js
@@ -15,6 +15,16 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 import * as directives from '@/directives'
+
+import Component from '@/components'
+Vue.use(Component) // 注册自己的插件
+
+import * as filters from '@/filters' // 引入工具类
+// 注册全局的过滤器
+Object.keys(filters).forEach(key => {
+  // 注册过滤器
+  Vue.filter(key, filters[key])
+})
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
