470bb11eeb5210fb17d4e2faf38dd4697f7312f3
xuhui-web <578734857@qq.com>
2022-06-30 19:39:51 +0800

    提交删除，编辑公司角色

--- src/api/setting.js
+++ src/api/setting.js
@@ -20,3 +20,34 @@ export function getCompanyInfo(companyId) {
     url: `/company/${companyId}`
   })
 }
+
+/** **
+ *  删除角色
+ *
+ * ****/
+export function deleteRole(id) {
+  return request({
+    url: `/sys/role/${id}`,
+    method: 'delete'
+  })
+}
+
+/** *
+ * 修改角色
+ * ***/
+export function updateRole(data) {
+  return request({
+    url: `/sys/role/${data.id}`,
+    data,
+    method: 'put'
+  })
+}
+
+/**
+ * 获取角色详情
+ * **/
+export function getRoleDetail(id) {
+  return request({
+    url: `/sys/role/${id}`
+  })
+}
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -15,9 +15,11 @@
                             <el-table-column align="center" prop="name" label="名称" width="240" />
                             <el-table-column align="center" prop="description" label="描述" />
                             <el-table-column align="center" label="操作">
-                                <el-button size="small" type="success">分配权限</el-button>
-                                <el-button size="small" type="primary">编辑</el-button>
-                                <el-button size="small" type="danger">删除</el-button>
+                                <template slot-scope="{ row }">
+                                    <el-button size="small" type="success">分配权限</el-button>
+                                    <el-button size="small" type="primary" @click="editRole(row.id)">编辑</el-button>
+                                    <el-button size="small" type="danger" @click="deleteRole(row.id)">删除</el-button>
+                                </template>
                             </el-table-column>
                         </el-table>
 
@@ -32,6 +34,7 @@
                             />
                         </el-row>
                     </el-tab-pane>
+
                     <el-tab-pane label="公司信息">
                         <el-alert
                             title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
@@ -61,13 +64,31 @@
                         </el-form>
                     </el-tab-pane>
                 </el-tabs>
+
+                <el-dialog title="编辑弹层" :visible="showDialog" @close="btnCancel">
+                    <el-form ref="roleForm" :model="roleForm" :rules="rules" label-width="120px">
+                        <el-form-item label="角色名称" prop="name">
+                            <el-input v-model="roleForm.name" />
+                        </el-form-item>
+                        <el-form-item label="角色描述">
+                            <el-input v-model="roleForm.description" />
+                        </el-form-item>
+                    </el-form>
+                    <!-- 底部 -->
+                    <el-row slot="footer" type="flex" justify="center">
+                        <el-col :span="6">
+                            <el-button size="small" @click="btnCancel">取消</el-button>
+                            <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+                        </el-col>
+                    </el-row>
+                </el-dialog>
             </el-card>
         </div>
     </div>
 </template>
 
 <script>
-import { getRoleList, getCompanyInfo } from "@/api/setting"
+import { getRoleList, getCompanyInfo, deleteRole, updateRole, getRoleDetail } from "@/api/setting"
 import { mapGetters } from "vuex"
 
 export default {
@@ -81,6 +102,12 @@ export default {
                 total: 0, // 记录总数
             },
             formData: {},
+            showDialog: false,
+            // 专门接收新增或者编辑的编辑的表单数据
+            roleForm: {},
+            rules: {
+                name: [{ required: true, message: "角色名称不能为空", trigger: "blur" }],
+            },
         }
     },
     created() {
@@ -104,6 +131,45 @@ export default {
         async getCompanyInfo() {
             this.formData = await getCompanyInfo(this.companyId)
             console.log(this.formData)
+        },
+        async deleteRole(id) {
+            //  提示
+            try {
+                await this.$confirm("确认删除该角色吗")
+                // 只有点击了确定 才能进入到下方
+                await deleteRole(id) // 调用删除接口
+                this.getRoleList() // 重新加载数据
+                this.$message.success("删除角色成功")
+            } catch (error) {
+                console.log(error)
+            }
+        },
+
+        async editRole(id) {
+            this.roleForm = await getRoleDetail(id)
+            this.showDialog = true // 为了不出现闪烁的问题 先获取数据 再弹出层
+        },
+        async btnOK() {
+            try {
+                await this.$refs.roleForm.validate()
+                // 只有校验通过的情况下 才会执行await的下方内容
+                // roleForm这个对象有id就是编辑 没有id就是新增
+                if (this.roleForm.id) {
+                    await updateRole(this.roleForm)
+                } else {
+                    // 新增业务
+                }
+                // 重新拉取数据
+                this.getRoleList()
+                this.$message.success("操作成功")
+                this.showDialog = false
+            } catch (error) {
+                console.log(error)
+            }
+        },
+
+        btnCancel() {
+            this.showDialog = false
         },
     },
     computed: {
