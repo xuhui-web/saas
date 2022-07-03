323d3e880323293e4fa2eacbdef4f9e7fba275c4
xuhui-web <578734857@qq.com>
2022-07-03 14:23:25 +0800

    提交上传

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -14,6 +14,18 @@
                 <el-table border :data="list">
                     <el-table-column label="序号" sortable="" type="index" />
                     <el-table-column label="姓名" sortable="" prop="username" />
+                    <el-table-column label="头像" align="center">
+                        <template slot-scope="{ row }">
+                            <img
+                                slot="reference"
+                                v-imageerror="require('@/assets/common/bigUserHeader.png')"
+                                :src="row.staffPhoto"
+                                style="border-radius: 50%; width: 100px; height: 100px; padding: 10px"
+                                alt=""
+                                @click="showQrCode(row.staffPhoto)"
+                            />
+                        </template>
+                    </el-table-column>
                     <el-table-column label="工号" sortable="" prop="workNumber" />
                     <el-table-column
                         label="聘用形式"
@@ -58,6 +70,12 @@
                 </el-row>
             </el-card>
 
+            <el-dialog title="二维码" :visible.sync="showCodeDialog">
+                <el-row type="flex" justify="center">
+                    <canvas ref="myCanvas" />
+                </el-row>
+            </el-dialog>
+
             <!-- 放置新增组件 -->
             <add-employee :show-dialog.sync="showDialog" />
         </div>
@@ -67,6 +85,7 @@
 import { getEmployeeList, delEmployee } from "@/api/employees"
 import EmployeeEnum from "@/api/constant/employees"
 import AddEmployee from "./components/add-employee"
+import QrCode from "qrcode"
 export default {
     components: {
         AddEmployee,
@@ -81,6 +100,7 @@ export default {
                 total: 0, // 总数
             },
             showDialog: false,
+            showCodeDialog: false,
         }
     },
     created() {
@@ -94,6 +114,7 @@ export default {
         async getEmployeeList() {
             this.loading = true
             const { total, rows } = await getEmployeeList(this.page)
+
             this.page.total = total
             this.list = rows
             this.loading = false
@@ -114,6 +135,20 @@ export default {
                 this.$message.success("删除员工成功")
             } catch (error) {
                 console.log(error)
+            }
+        },
+        showQrCode(url) {
+            // url存在的情况下 才弹出层
+            if (url) {
+                this.showCodeDialog = true // 数据更新了 但是我的弹层会立刻出现吗 ？页面的渲染是异步的！！！！
+                // 有一个方法可以在上一次数据更新完毕，页面渲染完毕之后
+                this.$nextTick(() => {
+                    // 此时可以确认已经有ref对象了
+                    QrCode.toCanvas(this.$refs.myCanvas, url) // 将地址转化成二维码
+                    // 如果转化的二维码后面信息 是一个地址的话 就会跳转到该地址 如果不是地址就会显示内容
+                })
+            } else {
+                this.$message.warning("该用户还未上传头像")
             }
         },
     },
